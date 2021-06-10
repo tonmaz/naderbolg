54083b39b16da69663a285448ebd40ceb4e1c549
Tonmaz <54241333+tonmaz@users.noreply.github.com>
2021-06-10 12:21:44 +0200

    cleaned up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,5 @@
-import Link from "next/link";
+import { MainComp } from "@src/common/components/blocks/Main/mainComp";
 
 export default function Home() {
-  return (
-    <div>
-      <a
-        type="button"
-        className="inline-flex items-center py-2 px-3 text-xs font-medium text-white bg-blue-600 rounded border border-transparent shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        href="https://sellercentral-europe.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.69a50f4e-2ab0-4bdc-ad39-053396c104eb&version=beta"
-      >
-        Connect with Amazon here
-      </a>
-      <h1 className="mt-6 mb-2 text-3xl font-semibold tracking-wide"> Posts</h1>
-      My posts here
-    </div>
-  );
+  return <MainComp />;
 }
